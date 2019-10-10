3ffeee6bb4cb2413b8ea64ffa65e8e70a0dc2534
Evgeni Kunev <evgeni@uber.com>
2019-10-10 09:25:45 +0300

    Implement image loading of the given file name

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
 	"os"
 )
 
 func main() {
 	filePath := os.Args[1]
 	fmt.Printf("Attempting to read image from %s\n", filePath)
+	imageFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, format, err := image.Decode(imageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Read a %s image \n", format)
 	return
 }
