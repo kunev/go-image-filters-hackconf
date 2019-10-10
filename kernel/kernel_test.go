9871840ceb5964f8f3343930765ba281ffd06dbe
Evgeni Kunev <evgeni@uber.com>
2019-10-10 10:55:29 +0300

    Add test for Kernel.Apply

--- kernel/kernel_test.go
+++ kernel/kernel_test.go
@@ -1,6 +1,12 @@
 package kernel
 
-import "testing"
+import (
+	"image"
+	"os"
+	"testing"
+
+	_ "image/jpeg"
+)
 
 func TestNewKernel(t *testing.T) {
 	kernel := New([][]float32{
@@ -13,5 +19,22 @@ func TestNewKernel(t *testing.T) {
 	}
 	if kernel.Height != 3 {
 		t.Fatal("Height is wrong")
+	}
+}
+
+func TestKernel_Apply(t *testing.T) {
+	kernel := New([][]float32{
+		{0, 0, 0},
+		{0, 1, 0},
+		{0, 0, 0},
+	})
+	file, err := os.Open("../input.jpg")
+	image, _, err := image.Decode(file)
+	result, err := kernel.Apply(image)
+	if err != nil {
+		t.Fatal("kernel.Apply returner an error: ", err)
+	}
+	if result.Bounds() != image.Bounds() {
+		t.Fatal("result of kernel.Apply has different size")
 	}
 }
