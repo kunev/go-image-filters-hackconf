8c4faf85dc269e269397783942bbc3e01e5abe86
Evgeni Kunev <evgeni@uber.com>
2019-10-10 10:57:07 +0300

    Add getNeighbourhood method to Kernel

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,12 +1,21 @@
 package kernel
 
-import "image"
+import (
+	"image"
+	"image/color"
+)
 
 // Kernel describes an image kernel
 type Kernel struct {
 	Width        int
 	Height       int
 	Coefficients [][]float32
+}
+
+type neighbour struct {
+	xOffset int
+	yOffset int
+	clr     color.Color
 }
 
 // New returns a Kernel wrapping the given coefficients matrix
@@ -17,6 +26,27 @@ func New(coefficients [][]float32) Kernel {
 	result.Coefficients = coefficients
 
 	return result
+}
+
+func (k Kernel) getNeighbourhood(x, y int, img image.Image) []neighbour {
+	bounds := img.Bounds()
+	neighbourhood := []neighbour{}
+	for i := -k.Width / 2; i <= k.Width/2; i++ {
+		if x+i < bounds.Min.X || x+i > bounds.Max.X {
+			continue
+		}
+		for j := -k.Height / 2; j <= k.Height/2; j++ {
+			if y+j < bounds.Min.Y || y+j > bounds.Max.Y {
+				continue
+			}
+			neighbourhood = append(neighbourhood, neighbour{
+				xOffset: i,
+				yOffset: j,
+				clr:     img.At(x+i, y+j),
+			})
+		}
+	}
+	return neighbourhood
 }
 
 // Apply applies a kernel to an image returning the resulting image
